bbde3136e637360324da2696241b1e4c150f581b
toannguyenle <toan@me.com>
2014-11-11 12:22:11 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Postgresspractice::Application.config.secret_key_base = 'f8343041b2931f6d4ac090fe349984b49a6c8150fbc8e8db32f38579d39f71462f17c202ff508766f071a9ca66446c3096832f4090dd2bcbc535eb4ef6422a65'
